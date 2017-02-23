5c61a499d50c0c5ce3d557ce7ed632b275f2960a
chenmingqing <1983945348@qq.com>
2017-02-23 09:20:29 +0800

    synchronized 不同位置，同步锁粒度不同

---
+++ src/foo/Foo.java
@@ -0,0 +1,19 @@
+package foo;
+
+/**
+ * Created by Administrator on 2017/2/23.
+ */
+public class Foo {
+    private int x = 100;
+
+    public int getX() {
+        return x;
+    }
+
+    public synchronized int fix(int y) {
+        x = x - y;
+        System.out.println("线程"+Thread.currentThread().getName() + "运行结束，减少“" + y
+                + "”，当前值为：" + x);
+        return x;
+    }
+}
