2d8da417456def187b0bb27ed3079d5f68c1c778
chenmingqing <1983945348@qq.com>
2017-02-23 09:26:39 +0800

    同步代码块指定对象 锁是对象级的

--- src/foo/Foo.java
+++ src/foo/Foo.java
@@ -10,10 +10,19 @@ public class Foo {
         return x;
     }
 
-    public synchronized int fix(int y) {
+    /*public synchronized int fix(int y) {
         x = x - y;
         System.out.println("线程"+Thread.currentThread().getName() + "运行结束，减少“" + y
                 + "”，当前值为：" + x);
+        return x;
+    }*/
+
+    public int fix(int y) {
+        synchronized(this) {
+            x = x - y;
+        }
+            System.out.println("线程" + Thread.currentThread().getName() + "运行结束，减少“" + y
+                    + "”，当前值为：" + x);
         return x;
     }
 }
